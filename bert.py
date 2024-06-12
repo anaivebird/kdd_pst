dc16f86347a99b1afd9da74698becd8933105593
anaivebird <29398611+anaivebird@users.noreply.github.com>
2024-06-12 15:06:27 +0800

    Update bert.py

--- bert.py
+++ bert.py
@@ -739,8 +739,8 @@ def gen_kddcup_valid_submission_bert(model_name="scibert"):
 
 
 if __name__ == "__main__":
-    # prepare_train_test_data_for_bert()
+    prepare_train_test_data_for_bert()
     prepare_bert_input()
     train(model_name="scibert")
     # eval_test_papers_bert(model_name="scibert")
-    gen_kddcup_valid_submission_bert(model_name="scibert")
+    # gen_kddcup_valid_submission_bert(model_name="scibert")
